f11c674cfe28e9ec196c9336dcd893ef4d43ac61
Piotr Chełmicki <Piotr.Chelmicki@lppsa.com>
2018-07-08 22:02:54 +0200

    - move index.html to src

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -19,5 +20,10 @@ module.exports = {
         contentBase: path.join(__dirname, 'dist'),
         compress: false,
         port: 9000
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
 };
